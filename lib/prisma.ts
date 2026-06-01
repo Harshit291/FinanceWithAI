7a23240ec207b23ef6876703e90a3e09d1eb7515
Harshit Gupta <harshit.gupta_c@sandvik.com>
2026-06-01 19:25:31 +0530

    Prisma got very strict; we need to install Prisma 7 and perhaps call it directly from the config. We cannot pass it directly into the environment local, so that changed.
    Now I am able to see the website running in my local environment. Hopefully, it will run in the deployment on Vercel as well.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,11 @@
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/lib/generated/prisma/client";
 
 function createPrismaClient() {
-  return new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
-  });
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaPg(pool);
+  return new PrismaClient({ adapter });
 }
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
